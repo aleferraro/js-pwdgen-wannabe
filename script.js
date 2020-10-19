65b4b6e15dee4038f4d77c89e30b5c92cd0018d9
Alessandro Ferraro <ale_ferraro@libero.it>
2020-10-19 16:01:53 +0200

    Password generator
    Implemented scripts that make appear a suggested password

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = prompt("Ciao, qual'è il tuo nome?");
+
+var cognome = prompt("Qual'è il tuo cognome?");
+
+var colorePreferito = prompt("Qual'è il tuo colore preferito?");
+
+document.getElementById("password").innerHTML = nome + cognome + colorePreferito + '19';
