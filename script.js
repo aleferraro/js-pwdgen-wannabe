73888835cac3e21b4ca3615c3b7285bb4cc53f22
Alessandro Ferraro <ale_ferraro@libero.it>
2020-11-15 16:42:23 +0100

    Refactoring
    Add the instructions of exercise

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+/*
+Chiedi all'utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+
+Infine scrivi sulla pagina nomecognomecolorepreferito19
+*/
+
 var nome = prompt("Ciao, qual'è il tuo nome?");
 
 var cognome = prompt("Qual'è il tuo cognome?");
 
 var colorePreferito = prompt("Qual'è il tuo colore preferito?");
 
-document.getElementById("password").innerHTML = nome + cognome + colorePreferito + '19';
+document.getElementById("password").innerHTML = '<strong>La tua nuova password è: </strong>' + nome + cognome + colorePreferito + '19';
